c842091ad2e89a56c12d1f4dc9ebfbfb7d2dc7a9
Fathrhys <fadhilfathirahesya@gmail.com>
2026-05-12 10:03:32 +0700

    feat: menambah laporan array numerik dan asosiatif beserta dokumentasi berupa gambar

--- minggu-1/Selasa/array-variable.php
+++ minggu-1/Selasa/array-variable.php
@@ -1,9 +1,26 @@
 <?php 
 echo "Belajar ARRAY di PHP YEAAAYYEE";
 echo "\n";
+echo "\n";
 
 echo "Array numerik\n";
 $skillSet = ["PHP", "JavaScript", "Python"];
-echo "Skill pertama yang dipelajari adalah = " . $skillSet[0];
+echo "Skill pertama yang dipelajari adalah = " . $skillSet[0] . "\n";
+echo "Skill kedua yang di pelajari adalah = " . $skillSet[1] . "\n";
+
+echo "\n";
+echo "Array asosiatif\n";
+$hewan = [
+    "namahewan" => "Kity",
+    "jenishewan" => "Kambing",
+    "makananhewan" => "Rumput" 
+];
+
+echo "Detail Hewan\n";
+echo "\n";
+echo "Nama Hewan yaitu = " . $hewan["namahewan"] . "\n";
+echo "Jenis Hewan = " . $hewan["jenishewan"] . "\n";
+echo "Makanan Hewan nya adalah = " . $hewan["makananhewan"] . "\n";
+
 
 ?>
