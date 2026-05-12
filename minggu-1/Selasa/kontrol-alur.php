fb9b0e71189b0e463dd281222c370ed9578dfe1a
Fathrhys <fadhilfathirahesya@gmail.com>
2026-05-12 11:04:04 +0700

    feat: menambah laporan array boolean dengan melakukan cek TRUE/FALSE beserta dokumentasi berupa gambar

--- minggu-1/Selasa/kontrol-alur.php
+++ minggu-1/Selasa/kontrol-alur.php
@@ -22,7 +22,28 @@ if ($nilai >= 90) {
 }
 
 echo "\n---------------------------------\n";
+echo "\n";
 
+echo "Belajar kontrol alur Boolean di PHP YEYAAAYYEE\n";
+echo "\n---------------------------------\n";
+echo "Cek kondisi TRUE\n";
+$serverMenyala = true;
+$databaseKonek = false;
+
+if ($serverMenyala) {
+    echo "=> Status Server: Aktif. Siap menerima koneksi!";
+} else {
+    echo "=> Status Server: Mati. Segera periksa log server!";
+}
+echo "\n---------------------------------\n";
+
+echo "Cek kondisi FALSE\n";
+if ($databaseKonek) {
+    echo "=> Status Database: Aktif. Siap menerima data!";
+} else {
+    echo "Status Database: Mati. Segera periksa Database!";
+}
+echo "\n---------------------------------\n";
 
 
 
